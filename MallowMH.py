607ef4d8317c465ce5a389ecdf60f758e0c71434
hughed2 <hughed2@rpi.edu>
2015-01-07 21:08:57 -0500

    Adding in support for non-uniform priors

--- MallowMH.py
+++ MallowMH.py
@@ -108,20 +108,23 @@ class metropolis_hastings:
 
     #Determines whether to move to x_p or not
     #Standard implementation would have the distribution zero out when kt distance gets too high
-    #So instead of probabilities, we use the kendall-tau distance. Need to adapt here.
-    def move_kendalltau(self, kt_x, kt_x_p):
-        phi = params["phi"]
+    #So instead of probabilities, we use the kendall-tau distance. So we need to adapt the move function.
+    def move_kendalltau(self, kt_x, kt_x_p, ratio):
+        if params["phi"]:
+            phi = params["phi"]
+        else:
+            phi = 0.2
         if(kt_x_p < kt_x): #Always move if 
             return True
-        if(random.random() < pow(phi, kt_x_p - kt_x)):
+        if(random.random()*ratio < pow(phi, kt_x_p - kt_x)):
             return True
         return False
 
-    #The default function uses actual probabilities
-    def move_default(self, prob_x, prob_x_p):
+    #The default move function uses actual probabilities
+    def move_default(self, prob_x, prob_x_p, ratio):
         if(prob_x_p > prob_x):
             return True
-        if(random.random() < prob_x_p / prob_x):
+        if(random.random()*ratio < prob_x_p / prob_x):
             return True
         return False
 
@@ -136,44 +139,40 @@ class metropolis_hastings:
             for j in range(i+1, k):
                 x[i][j] = random.random() > 0.5
         return x
+        
+    #Default the prior distribution to uniform
+    def uniform_prior(self, sampleA, sampleB):
+        return 1
 
-    #Set various parameters
-    def set_transitions(self, function):
-        functions = {"swap_neighbors" : self.transition_swap_neighbors}
-        self.transition = functions[function]
-
-    def set_loss(self, function):
-        functions = {"kendall-tau" : kendalltau}
-        self.loss = functions[function]
-
-    def set_distribution(self, function):
-        functions = {"kendall-tau": kendalltau_table}
-        self.distribution = functions[function]
-
-    def set_move(self, function):
-        functions = {"default" : self.move_default, "kendall-tau" : self.move_kendalltau}
-        self.should_move = functions[function]
-
+    #Set random parameter to L(C) or B(C)
     def set_random(self, function):
         functions = {"L(C)" : self.get_random_L, "B(C)" : self.get_random_B}
         self.get_random = functions[function]
 
-    transition = transition_swap_neighbors
-    loss = kendalltau
-    distribution = kendalltau_table
-    should_move = move_kendalltau
     get_random = get_random_L
+    prior = uniform_prior
 
     #Given a preference table, run the M-H algorithm
-    def approximate(self, preferences, k, parameters, trans_f="swap_neighbors", loss_f="kendall-tau",
-                                                dist_f="kendall-tau", space="L(C)"):
-        self.set_transitions(trans_f) #Set all of our parameters and functions
-        self.set_loss(loss_f)
-        self.set_distribution(dist_f)
-        if(dist_f == "kendall-tau"):
-            self.set_move("kendall-tau")
+    def approximate(self, preferences, k, parameters, trans_f="", loss_f="", prior_f="",
+                                                dist_f="", move_f = "", space="L(C)"):
+        if trans_f:
+            self.transition = trans_f #Set all of our parameters and functions
         else:
-            self.set_move("default")
+            self.transition = self.transition_swap_neighbors
+        if loss_f:
+            self.loss = loss_f
+        else:
+            self.loss = kendalltau
+        if dist_f:
+            self.distribution = dist_f
+        else:
+            self.distribution = kendalltau_table
+        if move_f:
+            self.should_move = move_f
+        else:
+            self.should_move = self.move_kendalltau
+        if prior_f:
+            self.prior = prior_f
         self.set_random(space)
         
 
@@ -196,7 +195,8 @@ class metropolis_hastings:
             
             #Calculate the acceptance rate and determine whether to step
             prob_x_p = self.distribution(x_p, preferences)
-            if(self.should_move(prob_x, prob_x_p)):
+            ratio = self.prior(x, x_p)
+            if(self.should_move(prob_x, prob_x_p, ratio)):
                 (x, prob_x) = (x_p, prob_x_p)
 
         return self.loss(ground, x)
